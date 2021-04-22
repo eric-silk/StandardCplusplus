6d9dfbee92965dcc91808af58ee3488b9c8f3204
VioletGiraffe <violet.giraffe@noreply.github.com>
2021-04-22 17:23:42 +0300

    std::size() implemented

---
+++ bits/std_size.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "../cstddef"
+
+namespace std {
+
+	template <class C> 
+	constexpr auto size(const C& c) -> decltype(c.size())
+	{
+		return c.size();
+	}
+
+	template <class T, std::size_t N>
+	constexpr std::size_t size(const T (&array)[N]) noexcept
+	{
+		return N;
+	}
+}
